b33c4d3d231d3e4098f7d757d69066085a77af05
rakshithreddykamasani-maker <rakshithreddykamasani@gmail.com>
2026-06-12 05:26:29 +0000

    hi

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("Welcome to Streamlit")
+
+name = st.text_input("Enter your name")
+
+if name:
+    st.success(f"Hello {name}")
+
+age = st.slider("Select your age", 1, 100)
+
+st.write("Age:", age)
+
+if st.button("Celebrate"):
+    st.balloons()
